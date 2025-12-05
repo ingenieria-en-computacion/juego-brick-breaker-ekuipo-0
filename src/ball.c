42f8de1f70275c5ca78b9984bf1ddbb9d125a377
fermu06_635 <fernandamg635@gmail.com>
2025-12-05 00:25:14 -0600

    Actualización ball.c
    Initialize ball position and set random velocity.

--- src/ball.c
+++ src/ball.c
@@ -1,13 +1,41 @@
 #include "ball.h"
+#include <stdlib.h>
+#include <time.h>
+
+#define WIDTH 80
+#define HEIGHT 24
 
 static Ball ball;
 
 void ball_init() {
-    // TODO: posicionar la pelota al centro y darle velocidad inicial
+    // Posicionar la pelota al centro
+    ball.x = WIDTH / 2;
+    ball.y = HEIGHT / 2;
+    
+    // Dar velocidad inicial (aleatoria)
+    ball.dx = (rand() % 2) ? 1 : -1;
+    ball.dy = -1;  // Hacia arriba
 }
 
 void ball_update() {
-    // TODO: mover la pelota y actualizar colisiones generales
+    // Mover la pelota
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+    
+    // Rebote en paredes laterales
+    if (ball.x <= 0) {
+        ball.x = 0;
+        ball.dx = 1;
+    } else if (ball.x >= WIDTH - 1) {
+        ball.x = WIDTH - 1;
+        ball.dx = -1;
+    }
+    
+    // Rebote en techo
+    if (ball.y <= 0) {
+        ball.y = 0;
+        ball.dy = 1;
+    }
 }
 
 Ball* ball_get() {
