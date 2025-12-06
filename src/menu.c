cf7d024cf071507d5a7ab5d638c59b5eb49455d2
Edel-morales <edelmilagros17@gmail.com>
2025-12-06 02:14:53 -0600

    menu.c actualizacdo :0

--- src/menu.c
+++ src/menu.c
@@ -3,13 +3,28 @@
 
 // TODO: El estudiante debe completar el menú
 void menu_show() {
+    // Limpiar pantalla
+    printf("\033[2J\033[H");
+
     printf("\n======== BRICK BREAKER ========\n");
     printf("1) Jugar\n");    
-    printf("Seleccione una opción: ");
+    printf("2) Instrucciones\n");
+    printf("3) Salir\n");
+    printf("=================================\n");
+    printf("Seleccione una opcion (1-3): ");
 }
 
 int menu_get_option() {
     int opt;
-    // TODO: Completar la lectura de la opción. 
-    return opt;
+    // Lee la opción ingresada y devuelve su valor, o -1 si es inválida.
+    char buffer[100];
+    
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        if (sscanf(buffer, "%d", &opt) == 1) {
+            return opt;
+        }
+    }
+    
+    return -1;  // Opción inválida
 }
+
